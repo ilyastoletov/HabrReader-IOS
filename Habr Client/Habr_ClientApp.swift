39ab8c85d8ec64ee4eda328b39523fd808f1bc80
Ilya <ilastoletov7@gmail.com>
2024-02-14 13:56:01 +0400

    Initial Commit

---
+++ Habr Client/Habr_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habr_ClientApp.swift
+//  Habr Client
+//
+//  Created by Ilya Stoletov on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Habr_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
